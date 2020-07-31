f609a5d751278c70e69eff9d0567945cfb503bcf
SamuelEdward <samueledward40@gmail.com>
2020-07-31 08:15:17 -0700

    type

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 jQuery(document).ready(function(){
 
 $(".text").typed({
-    strings:["I am Talent Planet"],
-    typedspeed:0,
+    strings:["I am Talent Planet","Second setence"],
+    typespeed:0,
 });
 
 });
