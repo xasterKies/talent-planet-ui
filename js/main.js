aeb752db5011f73911f27cef64e47ed43c5c6216
SamuelEdward <samueledward40@gmail.com>
2020-07-31 08:37:43 -0700

    t

--- js/main.js
+++ js/main.js
@@ -1,8 +1,5 @@
-jQuery(document).ready(function(){
 
-$(".text").typed({
-    strings:["I am Talent Planet","Second setence"],
-    typespeed:0,
-});
-
-});
+var typed = new Typed('.text', {
+    strings: ['This is a JavaScript library', 'This is an ES6 module'],
+    smartBackspace: true // Default value
+  });
