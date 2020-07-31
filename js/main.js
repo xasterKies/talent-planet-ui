fc99945b8fe2e19abc5cac32eb5e1b48bc65474d
SamuelEdward <samueledward40@gmail.com>
2020-07-31 09:14:36 -0700

    primay

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 
 var typed = new Typed('.text', {
     // Waits 1000ms after typing "First"
-    strings: ['^500I am ^1000Talent Planet^1000'],
+    strings: ['^500I am ^1000<span class='primary'>Talent Planet</span>^1000'],
     typeSpeed: 40,
     loop: true
   });
