e01f48c27688cac9232c41de2edffb6cfaf0288a
SamuelEdward <samueledward40@gmail.com>
2020-07-31 09:04:23 -0700

    di

--- js/main.js
+++ js/main.js
@@ -1,12 +1,7 @@
 
 var typed = new Typed('.text', {
     // Waits 1000ms after typing "First"
-    strings: ['I am ^1000 Talent Planet.', 'I am ^1000 Talent Planet.', 
-    'I am ^1000 Talent Planet.',
-     'I am ^1000 Talent Planet.', 'I am ^1000 Talent Planet.'
-     , 'I am ^1000 Talent Planet.',
-      'I am ^1000 Talent Planet.', 
-      'I am ^1000 Talent Planet.'],
+    strings: ['I am ^1000 Talent Planet. ^1000'],
     typeSpeed: 40,
     loop: true
   });
