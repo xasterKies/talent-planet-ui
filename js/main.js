70557b1433d6ba91354c6412196419368e5aa66e
SamuelEdward <samueledward40@gmail.com>
2020-07-31 08:53:43 -0700

    text

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 
 var typed = new Typed('.text', {
     // Waits 1000ms after typing "First"
-    strings: ['^1000 I am ^1000 Talent Planet.^1000', '^1000 I am ^1000 Talent Planet.^1000'],
-    typeSpeed: 40,
+    strings: ['I am ^1000 Talent Planet.^1000', '^1000 I am ^1000 Talent Planet.^1000'],
+    typeSpeed: 70,
     loop: true
   });
