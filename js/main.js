2ac53c5c43a7a429d14b5a2ca5b282b359cedab7
SamuelEdward <samueledward40@gmail.com>
2020-07-31 09:47:38 -0700

    d

--- js/main.js
+++ js/main.js
@@ -3,11 +3,13 @@ jQuery(document).ready({
         
         var top = $(window).scrollTop();
         if(top >= 60){
-            $("nav").addClass('secondary')
+            $("nav").addClass('secondary');
         }
 
         else
-        
+            if($("nav").hasClass('secondary')){
+                $("nav").removeClass('secondary')
+            }
 
     });
 
