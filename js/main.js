17f2aa6bff792e3404acfac94a119e3cbb8c58a9
SamuelEdward <samueledward40@gmail.com>
2020-08-07 01:39:10 -0700

    owl

--- js/main.js
+++ js/main.js
@@ -37,6 +37,16 @@ $('owl-carousel').owlCarousel({
     loop: true,
     margin: 10,
     nav: true,
-    responsi
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
 })
 
