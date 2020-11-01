352709c9af5cab85f3f0c0e10e02326549e640d4
Peng Boris Akebuon <peng.akebuon2468@gmail.com>
2020-11-01 20:27:56 +0100

    created templates folder and events template

--- events/views.py
+++ events/views.py
@@ -5,4 +5,4 @@ from .models import Event_Type, Event
 class HomeView(ListView):
     model = Event
     paginate_by = 3
-    template_name = "events/index.html"
+    template_name = "events/events.html"
