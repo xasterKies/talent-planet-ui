5a6ae6db46ad527b1fcdd8313b3d622f3535b6c1
Peng Boris Akebuon <peng.akebuon2468@gmail.com>
2020-11-01 20:33:42 +0100

    set static files urls in settings

--- Talent_Planet/settings.py
+++ Talent_Planet/settings.py
@@ -116,6 +116,12 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.1/howto/static-files/
+# https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root/')
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'all_statics')]
