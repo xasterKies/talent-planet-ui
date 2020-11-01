b9bc89cf977f54ea4dca4b0fe5c873e11660ff52
Peng Boris Akebuon <peng.akebuon2468@gmail.com>
2020-11-01 19:49:58 +0100

    created event_type and event models

--- Talent_Planet/settings.py
+++ Talent_Planet/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'events.apps.ShopConfig',
+    'events.apps.EventsConfig',
 ]
 
 MIDDLEWARE = [
--- events/models.py
+++ events/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+class Event_Type(models.Model):
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, unique=True)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'event_type'
+        verbose_name_plural = 'event_types'
+
+    def __str__(self):
+        return self.name
+
+class Event(models.Model):
+    event_type = models.ForeignKey(Event_Type,
+                                related_name='events',
+                                on_delete=models.CASCADE)
+    name = models.CharField(max_length = 200, db_index=True)
+    slug = models.SlugField(max_length = 200, db_index=True)
+    image = models.ImageField(upload_to='gallery/%Y/%m', blank=True)
+    description = models.TextField(blank=True)
+    facebook_link = models.URLField(max_length=200)
+    whatsapp_link = models.URLField(max_length=200)
+    instagram_link = models.URLField(max_length=200)
+    event_link = models.URLField(max_length=200)
+
+    class Meta:
+        ordering = ('name',)
+        index_together = (('id', 'slug'),)
+        #for queries involving id and slug
+
+    def __str__(self):
+        return self.name
