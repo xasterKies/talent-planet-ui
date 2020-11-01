abbe64d5ff0d1109525db4e38de16935cd92735d
Peng Boris Akebuon <peng.akebuon2468@gmail.com>
2020-11-01 20:02:38 +0100

    added event models to admin site

--- events/admin.py
+++ events/admin.py
@@ -1,1 +1,18 @@
 from django.contrib import admin
+from .models import Event_Type, Event
+
+
+
+@admin.register(Event_Type)
+class Event_TypeAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'event_link', 
+                    'created', 'active']
+                    
+    list_filter = ['name', 'created']
+    list_editable = ['event_link', 'active']
+    prepopulated_fields = {'slug': ('name',)}
